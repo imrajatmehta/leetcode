af29666155e3d315cd7b546a9a30b39259e5ceee
Rajat Mehta <rajatmehta@mac.local>
2022-12-05 12:04:09 +0530

    https://leetcode.com/problems/middle-of-the-linked-list/submissions/

---
+++ solution/middle-of-the-linked-list.go
@@ -0,0 +1,12 @@
+package solution
+
+func MiddleNode(head *ListNode) *ListNode {
+	slow := head
+	fast := head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return slow
+
+}
