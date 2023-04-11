60948ceb16dbebbbded48a999be9bd7329d235e3
Rajat Mehta <rajatmehta@mac.local>
2023-04-11 22:40:43 +0530

    https://leetcode.com/problems/valid-parentheses/submissions/

--- solution/valid-parentheses.go
+++ solution/valid-parentheses.go
@@ -5,16 +5,22 @@ import "github.com/imrajatmehta/leetcode/library"
 func IsValid(s string) bool {
 	stack := library.New()
 	for i := range s {
+
 		if s[i] == '(' || s[i] == '[' || s[i] == '{' {
 			stack.Push(s[i])
-		} else if s[i] == '}' && stack.Peek().(byte) == '{' {
-			stack.Pop()
-		} else if s[i] == ']' && stack.Peek().(byte) == '[' {
-			stack.Pop()
-		} else if s[i] == ')' && stack.Peek().(byte) == '(' {
-			stack.Pop()
 		} else {
-			return false
+			if stack.Len() <= 0 {
+				return false
+			}
+			if s[i] == '}' && stack.Peek().(byte) == '{' {
+				stack.Pop()
+			} else if s[i] == ']' && stack.Peek().(byte) == '[' {
+				stack.Pop()
+			} else if s[i] == ')' && stack.Peek().(byte) == '(' {
+				stack.Pop()
+			} else {
+				return false
+			}
 		}
 	}
 	return stack.Len() <= 0
